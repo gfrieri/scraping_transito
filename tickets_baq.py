2bdebc95dc40a520c5b6d2387e32de0b2f6f5ef3
Giuliano Frieri <55295148+gfrieri@users.noreply.github.com>
2024-11-27 12:26:46 -0500

    Testing

--- tickets_baq.py
+++ tickets_baq.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
-import re
+import time
 
 driver = webdriver.Edge()
 driver.get('https://portal.barranquilla.gov.co:8181/ConsultaEstadoCuenta/consultaPlaca')
@@ -20,33 +20,28 @@ while True:
     try:
         ticket_html_id = f'form:tblelectronicos:{ticket_row}:j_idt46'
         
-        # Wait for the ticket to be clickable
         ticket_element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, ticket_html_id)))
         ticket_element.click()
 
-        # Wait for the ticket details modal to load
-        ticket_details_modal = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'ui-dialog-content')))
+        ticket_details_modal = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'form:j_idt89')))
         print(f"Details for ticket {ticket_row}:", ticket_details_modal.text)
 
-        # Append the modal content to the list
         ticket_info.append(ticket_details_modal.text)
-
-        # Close the modal by clicking the "X" button
-        close_button = driver.find_element(By.CLASS_NAME, 'ui-icon ui-icon-closethick')
-        close_button.click()
-
-        # Wait briefly before processing the next ticket
-        WebDriverWait(driver, 1)
         
-        # Increment the row index
+        close_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a.ui-dialog-titlebar-close")))
+        driver.execute_script("arguments[0].click();", close_button)
+        #close_button = driver.find_element(By.CSS_SELECTOR, "a.ui-dialog-titlebar-close")
+        #Wclose_button.click()
+
+        WebDriverWait(driver, 10).until_not(EC.visibility_of_element_located((By.ID, 'form:j_idt89')))
+
         ticket_row += 1
         
     except Exception as e:
-        print(e)
+        print(f"Stopping at ticket {ticket_row}: {e}")
         break
 
 driver.quit()
 
-# Convert the list of ticket details to a DataFrame
 df = pd.DataFrame(ticket_info, columns=['Ticket Details'])
 print(df)
